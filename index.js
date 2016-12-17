48e84d60487a174e9ea4fa890d70535ff2688ed2
freddieb <freddieiabrown@hotmail.co.uk>
2016-12-17 14:45:07 +0000

    Connected DB

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ var express = require("express")
 var app = express();
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
+var config = require('./config/config.js')
 
-server.listen(5050);
+server.listen(config.host);
+
+mongoose.connect(config.dbURL);
+require('./models');
 
 io.on("connection", function (socket){
   socket.emit("message", "testing");
