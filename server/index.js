1a915e0451f77a83f66ccaee89e548279c1ad6df
freddieb <freddieiabrown@hotmail.co.uk>
2016-12-17 15:12:52 +0000

    Fix

--- server/index.js
+++ server/index.js
@@ -2,9 +2,10 @@ var express = require("express")
 var app = express();
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
-var config = require('./config/config.js')
+var config = require('./config/config.js');
+var mongoose = require('mongoose');
 
-server.listen(config.host);
+server.listen(config.listen.port, config.listen.hostname);
 
 mongoose.connect(config.dbURL);
 require('./models');
@@ -13,4 +14,4 @@ io.on("connection", function (socket){
   socket.emit("message", "testing");
 });
 
-app.use("/", express.static("frontend"))
+app.use("/", express.static("../frontend"))
