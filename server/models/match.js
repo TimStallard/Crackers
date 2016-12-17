fb3de4dd01de7f762c557d4563ac4cac2bd9799c
freddieb <freddieiabrown@hotmail.co.uk>
2016-12-17 15:02:42 +0000

    Models and Config update

---
+++ server/models/match.js
@@ -0,0 +1,21 @@
+// User model
+var mongoose = require('mongoose')
+
+var matchSchema = mongoose.Schema({
+	initiator: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User',
+		requried: true
+	},
+	partner: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User',
+		required: true
+	},
+    winner: {
+        type: mongoose.Schema.Types.ObjectId,
+		ref: 'User'
+    }
+});
+
+module.exports = mongoose.model('Match', matchSchema);
--- server/models/user.js
+++ server/models/user.js
@@ -1,1 +1,21 @@
 // User model
+var mongoose = require('mongoose')
+
+var userSchema = mongoose.Schema({
+	name: {
+		type: String,
+        required: true
+	},
+	UUID: {
+		type: String,
+        required: true,
+        unique: true,
+        dropDups: true
+	},
+    created: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
